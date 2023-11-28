273fe645bcb2275fe9d2d4b095d104ce20d4bed0
chiuycp <chiuyc2002@gmail.com>
2023-11-28 17:00:53 +0800

    transform door scripts

---
+++ Assets/Scripts/TransformDoor1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformDoor1 : MonoBehaviour
+{
+
+    private float timer;
+    public GameObject aimdoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other);
+        if(TransformDoor_Controller.doorTrigger(1)>2.0f) {
+            other.gameObject.transform.position = aimdoor.transform.position;
+            // timer = 0;
+            Debug.Log("Transform1");
+        }
+    }
+}
--- Assets/Scripts/ch3_game_manager.cs
+++ Assets/Scripts/ch3_game_manager.cs
@@ -40,10 +40,18 @@ public class ch3_game_manager : MonoBehaviour
     private Tiles blueTiles;
     [SerializeField]
     // private ImageFade imageFade;
+    private static float timer;
+    private static bool the_first_transform = true;
     private static ch3_game_manager instance = null;
     private void Awake() {
         instance = this;
+        timer = 0;
         blueTiles.Disable();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
     }
     private void Start() {
         // imageFade.SetColor(Color.black);
@@ -54,7 +62,24 @@ public class ch3_game_manager : MonoBehaviour
         instance.blueTiles.Toggle();
         // blueTiles.Toggle();
     }
+
+    // public static float doorTrigger() {
+    //     float return_time;
+    //     if(the_first_transform) {
+    //         return_time = 5.0f;
+    //         timer = 0;
+    //         the_first_transform = false;
+    //     }
+    //     else {
+    //         return_time = timer;
+    //         if(timer > 3.0) timer = 0;
+    //     }
+        
+    //     return return_time ;
+    // }
     public static void Restart(){
+        // timer = 0;
+        // the_first_transform = true;
         var task = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
         task.allowSceneActivation = false;
         // instance.imageFade.StartFade(Color.black, 1.0f, () => {

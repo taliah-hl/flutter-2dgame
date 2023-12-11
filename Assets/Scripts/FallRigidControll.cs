58e6d92f9c555c891860b2bc916997a7ad012fda
chiuycp <chiuyc2002@gmail.com>
2023-12-11 17:19:55 +0800

    ch4-1 ch4-2

---
+++ Assets/Scripts/FallRigidControll.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallRigidControll : MonoBehaviour
+{
+    private bool rigidon = false;
+    
+    private void Awake() {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag=="Player" && !rigidon) {
+            // UnityEngineInternal.APIUpdaterRuntimeServices.AddComponent(gameObject, "Assets/Scripts/FallRigidControll.cs (22,13)", "RigidBody2D");
+            Invoke("AddRigid", 5);
+        }
+        
+    }
+
+    void AddRigid() {
+        gameObject.AddComponent<Rigidbody2D>();
+        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+}
+
+
+
+

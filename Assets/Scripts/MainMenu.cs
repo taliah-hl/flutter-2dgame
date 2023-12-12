216b3ade8675ed7f522e088966135ea1d212c613
stevennnccc <92087060+stevennnccc@users.noreply.github.com>
2023-12-13 07:23:01 +0800

    Sticky Platform Script Added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void LoadChapter1_1()
+    {
+        SceneManager.LoadScene("ch1-1");
+    }
+
+    public void LoadChapter1_2()
+    {
+        SceneManager.LoadScene("ch1-2");
+    }
+    public void LoadChapter1_3()
+    {
+        SceneManager.LoadScene("ch1-3");
+    }
+    public void LoadChapter2_1()
+    {
+        SceneManager.LoadScene("ch2-1");
+    }
+    public void LoadChapter2_2()
+    {
+        SceneManager.LoadScene("ch2-2");
+    }
+    public void LoadChapter2_3()
+    {
+        SceneManager.LoadScene("ch2-3");
+    }
+    public void LoadChapter3_1()
+    {
+        SceneManager.LoadScene("ch3-1");
+    }
+    public void LoadChapter3_2()
+    {
+        SceneManager.LoadScene("ch3-2");
+    }
+    public void LoadChapter3_3()
+    {
+        SceneManager.LoadScene("ch3-3");
+    }
+    public void LoadChapter4_1()
+    {
+        SceneManager.LoadScene("ch4-1");
+    }
+    public void LoadChapter4_2()
+    {
+        SceneManager.LoadScene("ch4-2");
+    }
+    public void LoadChapter4_3()
+    {
+        SceneManager.LoadScene("ch4-3");
+    }
+}

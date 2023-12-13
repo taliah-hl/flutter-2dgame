73e36525bb51d3c3634f4ce62ec684894841a135
taliah-hl <taliah-hl-1111@protonmail.com>
2023-12-13 23:26:58 +0800

    fine tune ch2

--- Assets/Scripts/StickyPlatform.cs
+++ Assets/Scripts/StickyPlatform.cs
@@ -7,7 +7,7 @@ public class StickyPlatform : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" || other.gameObject.name=="stickCloud")
         {
             other.gameObject.transform.SetParent(transform);
         }
